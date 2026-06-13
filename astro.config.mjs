aea628429b4a9243b764c04bc14bdcbf7f2bf23a
Samir Bhowmik <bhowmik.samir.r@gmail.com>
2026-06-13 12:14:11 +0000

    Configure for github.io root deployment; stash custom-domain CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://samirbhowmik.cc',
+  site: 'https://samirbhowmik.github.io',
   trailingSlash: 'ignore',
   build: {
     format: 'directory',
